7c5480f4adc0b521f3faf8ecdfc0d0a6db1f1726
audrey <amonaaudrey16@gmail.com>
2023-06-07 09:12:25 +0000

    ajouts de taches

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    <h4 class="title">{{ name }}'s To Do List</h4>
+    <h4 class="title">La TO DO liste de {{ nom }}</h4>
+    <div class="container-task">
+      <div class="ligne">
+        <div class="titre-colonne-1">Tâche</div>
+
+        <div class="titre-colonne-1">Effectuée</div>
+      </div>
+      <div class="ligne" v-for="task in tasks" v-bind:key="task.action">
+        <div class="colonne-1">{{ task.action }}</div>
+
+        <div class="colonne-2">{{ task.faite }}</div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -8,21 +20,38 @@ export default {
   name: "app",
   data() {
     return {
-      name: "Drey",
+      nom: "Drey",
+      tasks: [
+        { action: "Finir le diagramme de classes", faite: false },
+        { action: "Concevoirla vidéo du projet", faite: false },
+        { action: "Debogger le code", faite: true },
+        { action: "Finir le Tp de Laravel", faite: false },
+      ],
     };
   },
 };
 </script>
 <style>
+/*app stylle*/
 #app {
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background: dodgerblue;
+  
 }
+/*app title style*/
 #app > h4.title {
+  background: dodgerblue;
+  width: 100%;
+  height: 100px;
   text-align: center;
   color: white;
   font-size: large;
+}
+/**task display style */
+#app .container-task{
+  padding: 50px;
+  width: 80%;
 }
 </style>
