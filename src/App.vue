1a06d464bc4960238d2aa15b4e217adb36a7238b
audrey <amonaaudrey16@gmail.com>
2023-06-07 08:40:06 +0000

    Ajout du premier style

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
   <div id="app">
-    <h4>To Do List</h4>
+    <h4 class="title">{{ name }}'s To Do List</h4>
   </div>
 </template>
 <script>
 export default {
   name: "app",
+  data() {
+    return {
+      name: "Audrey",
+    };
+  },
 };
 </script>
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: dodgerblue;
+}
+#app > h4.title {
+  text-align: center;
+  color: white;
+  font-size: large;
+}
+</style>
